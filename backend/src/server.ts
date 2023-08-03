9adaae46c8290abe835acb9d83de29b578dabdbf
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-03 17:53:52 -0300

    fea: create express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import { taskController } from "./controllers/tasks"
+import { initDatabase } from "./database/db"
 
 const app = express()
 const port = 3000
 
 app.use('/tasks', taskController)
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await initDatabase();
+
   console.log(`Example app listening on port ${port}`)
 })
