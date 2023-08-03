982d75bf422b9b796b1d8687d2587def612507f9
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-03 19:04:00 -0300

    fea: use middlewares on sever

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,20 @@
-import express from "express"
+import express, { Handler } from "express"
+import bodyParser from "body-parser"
 import { taskController } from "./controllers/tasks"
 import { initDatabase } from "./database/db"
+import { timeLogger } from "./middlewares"
 
 const app = express()
 const port = 3000
 
+// middlewares
+app.use(bodyParser)
+app.use(timeLogger)
+
+// endpoints
 app.use('/tasks', taskController)
 
+// init server
 app.listen(port, async () => {
   await initDatabase();
 
