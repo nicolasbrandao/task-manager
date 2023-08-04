0bec0af257921173bec1cae44b22a76e60e9bcc8
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-04 16:29:51 -0300

    fea: create base tasks list

--- frontend/app/components/TaskList.tsx
+++
@@ -1,5 +0,0 @@
-export default function TaskList() {
-  return (
-    <div className="bg-gray-900">TaskList</div>
-  )
-}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,9 @@
-import TaskList from "./components/TaskList";
+import TasksList from "../components/TasksList";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-black">
-      <TaskList />
+    <main className="flex min-h-screen flex-col items-center justify-between bg-neutral-800">
+      <TasksList />
     </main>
   )
 }
