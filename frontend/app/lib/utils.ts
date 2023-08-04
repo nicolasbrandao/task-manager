2c222297fe21d0021263b7744c83b6d93321b57c
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-04 16:59:27 -0300

    bui: rollback to vite

--- frontend/app/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { Task } from "@/types"
-
-const BASE_URL = "http://localhost:3000/tasks/"
-
-export async function fetchAllTasks(): Promise<Task[]> {
-  const tasks = await fetch(BASE_URL)
-  return tasks.json()
-}
--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TasksList from "../components/TasksList";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-neutral-800">
-      <TasksList />
-    </main>
-  )
-}
