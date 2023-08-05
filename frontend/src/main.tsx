331cc4f2b73e91c4f1adb13f0dab5c8e1eaa9c5d
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-05 19:11:44 -0300

    fea: add store provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import { ThemeProvider, createTheme } from '@mui/material'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
 
 const theme = createTheme({
   palette: {
+    mode: "dark",
     primary: {
       main: "#ff8f01",
+      dark: "#ff6201",
     },
-  }
+  },
+
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <CssBaseline>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </CssBaseline>
     </ThemeProvider>
   </React.StrictMode>
 )
