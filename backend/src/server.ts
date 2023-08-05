33458d1c92b40096d562d046af5e4dcd73447139
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-05 20:47:37 -0300

    fea: add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,15 @@ import bodyParser from "body-parser"
 import { taskController } from "./controllers/tasks"
 import { initDatabase } from "./database/db"
 import { timeLogger } from "./middlewares"
+import cors from "cors"
 
 const app = express()
 const port = 3000
 
 // middlewares
+app.use(cors())
+// TODO: use env var here
+app.use('http://localhost:5173/', cors())
 app.use(bodyParser.json())
 app.use(timeLogger)
 
