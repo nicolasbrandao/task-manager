865c965a30024394b15800e6d8e57d73e80c6f6c
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-05 22:01:45 -0300

    fix: remove unused fetch function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,9 +4,3 @@ export type Task = {
   description: string
 }
 
-const BASE_URL = "http://localhost:3000/tasks/"
-
-export async function fetchAllTasks(): Promise<Task[]> {
-  const tasks = await fetch(BASE_URL)
-  return tasks.json()
-}
