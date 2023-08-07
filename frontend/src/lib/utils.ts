21a098817c7f3c3c0e34f5cd3dfee7018d314710
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-07 17:32:50 -0300

    fea: add max char condition and error message

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const TaskSchema = z.object({
   id: z.string(),
-  title: z.string().min(1, { message: "Title is required" }),
-  description: z.string().min(1, { message: "Description is required" }),
+  title: z.string().min(1, { message: "Title is required" }).max(22, { message: "Title: max 22 characters" }),
+  description: z.string().min(1, { message: "Description is required" }).max(80, { message: "Description: max 80 characters" }),
 })
 
 export type Task = z.infer<typeof TaskSchema>;
