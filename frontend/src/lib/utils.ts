09a7d0d1f0eca4d0d955800596fba935a7da656a
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-07 16:52:14 -0300

    enh: include required error message to zod schema

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const TaskSchema = z.object({
   id: z.string(),
-  title: z.string(),
-  description: z.string(),
+  title: z.string().min(1, { message: "Title is required" }),
+  description: z.string().min(1, { message: "Description is required" }),
 })
 
 export type Task = z.infer<typeof TaskSchema>;
