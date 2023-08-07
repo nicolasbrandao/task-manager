51ea5a327c3e7ee7d58cfbe2f11e6a8c64155d90
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-07 16:53:59 -0300

    fea: implement debouncer on search

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,9 +1,12 @@
-import { Box, IconButton, Paper, TextField, Tooltip } from "@mui/material";
+import { Box, Paper, TextField } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import { useDispatch } from "react-redux";
 import { updateSearchingTerm } from "../store";
 import { ChangeEvent } from "react";
 import InputAdornment from '@mui/material/InputAdornment';
+import debounce from 'lodash.debounce'
+
+const DEBOUNCE_DELAY = 300 // ms
 
 export default function SearchBar() {
 
@@ -19,50 +22,50 @@ export default function SearchBar() {
         margin: "8px"
       }}
     >
-    <Box sx={{
-      margin: "8px",
-      display: "flex",
-      flexDirection: "column",
-      gap: "8px"
-    }}>
-      <form autoComplete="off">
-        <Box sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "8px"
-        }}>
+      <Box sx={{
+        margin: "8px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "8px"
+      }}>
+        <form autoComplete="off">
           <Box sx={{
             display: "flex",
-            flexDirection: "column",
-            gap: "8px",
-            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "8px"
           }}>
-            <TextField
-              onChange={handleChange}
-              variant="outlined"
-              size='small'
-              fullWidth
-              inputProps={{
-                maxLength: "22",
-              }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon 
-                      fontSize="inherit"
-                      sx={{
-                        color: "primary.main",
-                      }}
-                    />
-                 </InputAdornment>
-                )
-              }}
-            />
+            <Box sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "8px",
+              width: "100%",
+            }}>
+              <TextField
+                onChange={debounce(handleChange, DEBOUNCE_DELAY)}
+                variant="outlined"
+                size='small'
+                fullWidth
+                inputProps={{
+                  maxLength: "22",
+                }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon 
+                        fontSize="inherit"
+                        sx={{
+                          color: "primary.main",
+                        }}
+                      />
+                  </InputAdornment>
+                  )
+                }}
+              />
+            </Box>
           </Box>
-        </Box>
-      </form>
-    </Box>
+        </form>
+      </Box>
     </Paper>
   )
 }
