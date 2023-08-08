f330fc1ba6698650da5433fb8241f84383add66f
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-08 15:46:26 -0300

    enh: remove list action from model

--- backend/src/database/models/tasks.ts
+++ backend/src/database/models/tasks.ts
@@ -5,64 +5,45 @@ export const TaskSchema = z.object({
   id: z.string(),
   title: z.string(),
   description: z.string(),
-})
+});
 
 type Task = z.infer<typeof TaskSchema>;
 
-type TaskCollection = {
-  [task_id: string]: Omit<Task, 'id'>
-}
+type TaskCollection = Record<string, Omit<Task, "id">>;
 
-const COLLECTION_NAME = "tasks"
+const COLLECTION_NAME = "tasks";
 const collection = db.ref(COLLECTION_NAME);
 
 export class TaskDAO {
-  async create(task: TaskCollection[string]) {
+  async create(task: TaskCollection[string]): Promise<void> {
     await collection.push(task);
   }
 
-  async list(): Promise<Task[]> {
-    const snapshot = await collection.get<TaskCollection>();
-    const tasksCollection = snapshot.val() ?? {};
-
-    const tasks: Task[] = Object.entries(tasksCollection).map(
-      ([id, task]): Task => ({ id, ...task })
-    );
-
-    console.log({ tasks });
-    return tasks;
-  }
-
-  // TODO: check out search route
   async search(searchTerm: string): Promise<Task[]> {
-    const tasksSnapshotArray = await db.query(COLLECTION_NAME)
-      .filter('title', 'like', `*${searchTerm}*`)
+    const tasksSnapshotArray = await collection.query()
+      .filter("title", "like", `*${searchTerm}*`)
       .get();
 
-    console.log('Searching for titles with: ', searchTerm)
-    console.log(tasksSnapshotArray)
-  
-    const tasks: Task[] = [];
-    tasksSnapshotArray.forEach((snapshot) => {
-      const taskData: Omit<Task, 'id'> | null = snapshot.val();
-      if (taskData) {
-        tasks.push({ id: snapshot.key, ...taskData });
-      }
+    const tasks = tasksSnapshotArray.map((task) => {
+      return TaskSchema.parse({
+        id: task.key,
+        ...task.val() ?? {}
+      });
     });
 
     console.log(tasks);
     return tasks;
   }
 
-  async delete(id: string) {
-    await db.ref(`${COLLECTION_NAME}/${id}`).remove()
+  async delete(id: string): Promise<void> {
+    await db.ref(`${COLLECTION_NAME}/${id}`).remove();
   }
 
-  async update(updatedTask: Task) {
+  async update(updatedTask: Task): Promise<void> {
     const { id, title, description } = updatedTask;
     await db
       .ref(`${COLLECTION_NAME}/${id}`)
-      .update({ title, description })
+      .update({ title, description });
   }
 }
 
