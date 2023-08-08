fa38d6d8196c0af754b607d14f153fff3e916447
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-08 15:44:41 -0300

    enh: get allowed origins from .env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,27 @@
-import express from "express"
-import bodyParser from "body-parser"
-import { taskController } from "./controllers/tasks"
-import { initDatabase } from "./database/db"
-import { timeLogger } from "./middlewares"
-import cors from "cors"
+import express from "express";
+import bodyParser from "body-parser";
+import { taskController } from "./controllers/tasks";
+import { initDatabase } from "./database/db";
+import { timeLogger } from "./middlewares";
+import cors from "cors";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS;  // 'http://localhost:5173' TODO: pegar de env var
 
 // middlewares
-// TODO: use env var e conferir o cors para somente uma origem ou mais
-app.use(cors())
-app.use(bodyParser.json())
-app.use(timeLogger)
+// TODO: conferir o cors para somente uma origem ou mais
+app.use(cors());
+app.use(bodyParser.json());
+app.use(timeLogger);
 
 // endpoints
-app.use('/tasks', taskController)
+app.use("/tasks", taskController);
 
 // init server
 app.listen(port, async () => {
   await initDatabase();
 
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
