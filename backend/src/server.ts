0b3e3255b92db81edc4b970b5dadf25c9169e99c
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-08 16:51:49 -0300

    fix: remove unused env var

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,10 +8,8 @@ import cors from "cors";
 const app = express();
 const port = 3000;
 
-const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS;  // 'http://localhost:5173' TODO: pegar de env var
-
 // middlewares
-// TODO: conferir o cors para somente uma origem ou mais
+// TODO: config cors
 app.use(cors());
 app.use(bodyParser.json());
 app.use(timeLogger);
@@ -20,8 +18,12 @@ app.use(timeLogger);
 app.use("/tasks", taskController);
 
 // init server
-app.listen(port, async () => {
-  await initDatabase();
-
-  console.log(`Example app listening on port ${port}`);
-});
+initDatabase()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database initialization failed:", error);
+  });
